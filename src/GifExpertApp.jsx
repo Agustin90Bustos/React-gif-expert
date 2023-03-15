d3a04b8be3e166d63b9d62a9d5cbfd8ba1a05a92
Agustin <agu_mc@hotmail.com>
2023-03-15 16:59:45 -0300

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+import {AddCategory, GifGrid} from './components';
+
+
+export const GifExpertApp = () => {
+
+   const [categories, setCategories] = useState(['Saint Seiya']);
+
+   const onAddCategory = (newCategory) => {
+
+         if (categories.includes(newCategory)) return;
+
+         setCategories([newCategory, ...categories]);
+        //  categories.push(newCategory);
+         console.log(newCategory);
+   }
+
+    return (
+   
+    <>
+
+    <h1>GifExpertApp</h1>
+
+    <AddCategory 
+     onNewCategory={event => onAddCategory(event)}
+    />
+
+    {categories.map( (category) =>  (
+                    <GifGrid 
+                    key={ category}
+                    category={category}/>
+                    ))
+     }
+
+     </>
+           )
+    };
+
+
